718a0028d9a9fdfe8b9f0af38538ece53d87c336
sakshi-star <sakshigawali52@gmail.com>
2025-09-22 08:53:28 +0000

    Initial commit

---
+++ basics/variables.js
@@ -0,0 +1,20 @@
+const accountId = 12356
+let accountEmail = "sakshigawali52@gmail.com"
+var accountPassword = "1234567"
+accountCity = "Nashik"
+
+// accountId = 1 //not aalowed
+/* 
+prefer not to use var
+because of issue in block scope and functinonal scope
+*/
+
+accountEmail = "ankityadav"
+accountPassword = "234521"
+accountCity = "Malegaon"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+
+
