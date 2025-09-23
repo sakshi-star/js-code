c86099ea4f5eef9ede7475f72af1aafdceea8ea6
sakshi-star <sakshigawali52@gmail.com>
2025-09-23 16:16:36 +0000

    datatypes

--- basics/dataTypes.js
+++ basics/dataTypes.js
@@ -1,4 +1,31 @@
-"use strict"; // treat all js code newer version
+"use strict"; // treat all js code as newer version of js
 
-//alert( 3 + 3)  // we are using nodejs, not browser
+// alert( 3 + 3)  // we are using nodejs, not browser
 
+//console.log( 3 + 3) // code readeability should be high
+
+// console.log("Sakshi") 
+/* 
+tc39.es (ecma 2023) js documentation like it give us standards  
+*/
+
+let name ="sakshi" //string
+let age = "21" // number
+let isLoggedIn = false // boolean
+
+
+//* primitive dataTypes
+
+// number => 2 to power 53 ->  use to find range
+// bigint -> use for large number
+// string -> ""
+// boolean -> true/false
+// null -> standlone value and  it is a object
+// undefined -> not define any datatype
+// symbols -> unique
+
+
+
+//* Non-primitive DataType 
+
+//object
