fc1f772454ce51779b906f3bfd18061c11780f54
sakshi-star <sakshigawali52@gmail.com>
2025-09-26 16:32:21 +0000

    Prepare for push with LFS

---
+++ basics/comparison.js
@@ -0,0 +1,13 @@
+// (> <)
+// console.log(null > 0); false
+// console.log( null == 0);false
+// console.log( null >= 0); true
+
+// console.log( undefined == 0); all false
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+
+
+
