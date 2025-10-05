4f9e57d08e6377779660d1b8ec7b903e03d7a5d8
sakshi-star <sakshigawali52@gmail.com>
2025-10-05 12:50:45 +0000

    changes are done

--- advance/objects.js
+++ advance/objects.js
@@ -1,32 +1,32 @@
-// singleton -> was not creates by literals it creates by constructes
+//? singleton -> was not creates by literals it creates by constructes
 // Object.create -> it calleds constructors
 
 
-// object literals
+// *********object literals*************//
 
- const mySym = Symbol("key1") // -> symbol declared
+//  const mySym = Symbol("key1") // -> symbol declared
 
 
- const JsUser = {
-     name: "Sakshi",
-     [mySym]: "mykey", // syntax of print syntax
-     location: "Nashik",
-     isLoggedIn: false
- } // {}-> object
+//  const JsUser = {
+//      name: "Sakshi",
+//      [mySym]: "mykey", // syntax of print syntax
+//      location: "Nashik",
+//      isLoggedIn: false
+//  } // {}-> object
 
 //JsUser.email = "Yadavkayra" // use for change the values
 //Object.freeze(JsUser) // to freeze the all values
 //JsUser.email = "ankityadav"
 //console.log(JsUser);
 
-JsUser.greeting = function(){
-    console.log("Hello js users");
-}
-JsUser.greetingTwo = function(){
-    console.log(`Hello js users, ${this.name}`);
-}
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+// JsUser.greeting = function(){
+//     console.log("Hello js users");
+// }
+// JsUser.greetingTwo = function(){
+//     console.log(`Hello js users, ${this.name}`);
+// }
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
 
 
 
@@ -34,5 +34,80 @@ console.log(JsUser.greetingTwo());
 //console.log(JsUser.location);
 //console.log(typeof JsUser[mySym]);
 
+
+//*****Object Singleton************//
+  
+//const tinderUser = Object() // -> it is a singleton object
+
+const tinderUser = {}
+tinderUser.id = "123abcc"
+tinderUser.name = "sakhu"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "ankityadav",
+    fullname : {
+        usersfullname : {
+            firstname : "sakshi"
+            
+        }
+    }
+}
+console.log(regularUser.fullname.usersfullname);
+
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
+
+const returnedTarget = Object.assign({}, target, source);
+
+console.log(returnedTarget);
+// Expected output: Object { a: 1, b: 4, c: 5 }
+
+//console.log(returnedTarget === target);
+// Expected output: true
+
+const user = [
+    {
+    id : 1,
+    email : "sakshi@123.com"
+},
+{
+    id : 2,
+    email : "sakhu@123.com"
+},
+{
+    id : 3,
+    email : "ankit@123.com"
+}
+]
+user[1].email
+console.log(tinderUser);
+
+
+console.log(Object.keys(tinderUser)); //  important for when we create the projects
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser)); // display the array
+
+console.log(tinderUser.hasOwnProperty('isLogged')); // finds it has or not
+
+const course = {
+    coursename : "js code",
+    email : "@hiteshchoudary",
+    price : "1499"
+}
+
+const {price} = course
+
+console.log(price);
+
+
+
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "sakshi")
 
 
